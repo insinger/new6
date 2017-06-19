4e1413de3e398ec244e08198b7372fb03a583cee
insinger <insinger@entpsystems.com>
2017-06-19 14:35:19 +0800

    first working version

--- public/index.js
+++ public/index.js
@@ -1,1 +1,31 @@
 console.log("in index.js!");
+(function () {
+	var MyApp=angular.module("MyApp",[]);
+	myCtrl=this
+
+	MyCtrl=function () {
+		var myCtrl=this;
+		var upperCaseMode=false;
+		myCtrl.name="";
+		myCtrl.clearForm=function () {
+			console.log("in clearForm()!");
+			myCtrl.name="";
+			}
+		myCtrl.toggleCase=function () {
+			console.log("in toggleCase()!");
+			if (!upperCaseMode) {
+				myCtrl.name=myCtrl.name.toUpperCase();
+				upperCaseMode=true;
+				}
+			else {
+				myCtrl.name=myCtrl.name.toLowerCase();
+				upperCaseMode=false;
+				}
+			}
+		myCtrl.submitForm=function () {
+			console.log("in submitForm()!");
+			}
+		}
+	MyApp.controller("MyCtrl",MyCtrl);
+	}
+)();
--- serv.js
+++ serv.js
@@ -1,5 +1,5 @@
 var exp=require("express")
 var app=exp()
-app.use(express.static(__dirname+"/public")
-app.use(express.static(__dirname+"/bower_components")
+app.use(exp.static(__dirname+"/public"))
+app.use(exp.static(__dirname+"/bower_components"))
 app.listen(3000,()=>console.log("ready"))
